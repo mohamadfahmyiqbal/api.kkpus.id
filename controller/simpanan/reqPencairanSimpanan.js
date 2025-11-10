e1be543830b47d3c26b4e277e847c19faa07ee7f
mohamadfahnyiqbal <mohamadfahmyiqbal@gmail.com>
2025-11-10 09:16:43 +0000

    101125

--- controller/simpanan/reqPencairanSimpanan.js
+++ controller/simpanan/reqPencairanSimpanan.js
@@ -1,7 +1,35 @@
+import { MApprovalFlow } from "../../models/";
+
 export const reqPencairanSimpanan = async (req, res) => {
-  console.log(req.body);
-  console.log(req.anggota);
+  const { title, selected, jenis, type, back } = req.body;
+  const { nik, nama, email, roles } = req.anggota;
   try {
+    const getApproval = await MApprovalFlow.findAll({
+      where: { type: back },
+    });
+    const requestSave = {
+      token: selected[0].id,
+      nik,
+      tipe_request: type,
+      tipe_anggota: roles,
+    };
+
+    const transaksi = {
+      type,
+      jenis: back,
+      name: selected[0].name,
+      nik,
+      token: selected[0].id,
+      jumlah: selected[0].jumlah,
+      payment_status: "Menunggu Pembayaran",
+    };
+
+    const approval = {
+      token: selected[0].id,
+      requester_id: nik,
+      type: selected[0].name,
+    };
+    console.log({ req: requestSave, trans: transaksi });
   } catch (error) {
     console.log(error);
   }
